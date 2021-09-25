54b8e530e90c2449d38c699bdb861c7a3c9c18ed
dapo98 <dapobello98@gmail.com>
2021-09-25 05:10:04 +0000

    first commit

--- components/layout.js
+++ components/layout.js
@@ -1,30 +1,27 @@
-
 import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Layout( { children, home } ) {
   return (
-    
     <div>
       <Head>
         <title>Basic Next.js App</title>
       </Head>
       <header>
         <nav>
-          <a href="https://santarosa.edu">SRJC</a>
+          <a href="https://nextjs.org/">Next.js</a>
         </nav>
       </header>
       <main>{children}</main>
       {!home && (
           <Link href="/">
-            <a className="btn btn-primary mt-3">← Back </a>
+            <a className="btn btn-primary mt-3">← Back to home</a>
           </Link>
         )
       }
       <footer>
-        <p>Footer</p>
+        <p>The footer</p>
       </footer>
     </div>
-   
   );
 }
--- pages/[id].js
+++ pages/[id].js
@@ -1,11 +1,11 @@
-
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../components/layout';
 import { getAllIds, getData } from '../lib/data';
 
 export async function getStaticProps({ params }) {
   const itemData = await getData(params.id);
-  // console.log(itemData);
+  
   return {
     props: {
       itemData
@@ -13,7 +13,7 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticpaths() {
+export async function getStaticPaths() {
   const paths = getAllIds();
   return {
     paths,
@@ -24,7 +24,9 @@ export async function getStaticpaths() {
 export default function Entry({ itemData }) {
   return (
     <Layout>
+      {}
       <article className="card col-6">
+        <h2>Person Detail</h2>
         <div className="card-body">
           <h5 className="card-title">{itemData.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
@@ -32,6 +34,24 @@ export default function Entry({ itemData }) {
           <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
         </div>
       </article>
+      {}
+      <div className="list-group col-6">
+        {}
+        {itemData.related ? 
+          <h2>Related Persons</h2> : null
+        }
+        {itemData.related ? 
+          itemData.related.map(
+            ({ id, name }) => (
+              <Link key={id} href={`/${id}`}>
+                <a className="list-group-item list-group-item-action">{name}</a>
+              </Link>
+            )
+          )
+          : null
+        }
+        {}
+      </div>
     </Layout>
   );
 }
