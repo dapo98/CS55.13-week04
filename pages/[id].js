be477a6c4ad4b5b6975ea4378694ba4ca7966ead
dapo bello <dapobello98@gmail.com>
2021-12-02 22:18:43 -0800

    new changes

--- pages/[id].js
+++ pages/[id].js
@@ -28,10 +28,9 @@ export default function Entry({ itemData }) {
       <article className="card col-6">
         <h2>Person Detail</h2>
         <div className="card-body">
-          <h5 className="card-title">{itemData.name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
-          <p className="card-text">{itemData.birthdate}</p>
-          <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
+          <h5 className="card-title">{itemData.post_title}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.user_login}</h6>
+          <div classname="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}} /> </div>
         </div>
       </article>
       {}
